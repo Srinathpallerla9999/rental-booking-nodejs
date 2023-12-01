d20a9eefa5dae532834022e52b4e303a33743c44
Srinath Pallerla <srinathpallerla@digital-nirvana.com>
2023-12-01 09:44:49 +0530

    added comments

--- api/routes/postBooking.js
+++ api/routes/postBooking.js
@@ -5,15 +5,17 @@ const userDetails = require("../models/rentalBooking.js");
 
 const validatePayload = (req, res, next) => {
     try {
+        //here I have added validations to check allowed keys and other combinations
         const allowedKeys = ["firstname", "lastname", "vehicle_wheels", "vehicle_type", "vehicle_model", "start_date", "end_date"];
 
         const bodyKeys = Object.keys(req.body);
         const invalidKeys = bodyKeys.filter(key => !allowedKeys.includes(key.toLowerCase()));
         const missingKeys = allowedKeys.filter(key => !bodyKeys.includes(key.toLowerCase()));
-
+        //throws error if any key other than allowed key is added
         if (invalidKeys.length > 0) {
             return res.status(400).json({ error: `Invalid payload keys: ${invalidKeys.join(", ")}` });
         }
+         //throws error if any required allowed key is missed
         if (missingKeys.length > 0) {
             return res.status(400).json({ error: `Missing payload keys: ${missingKeys.join(", ")}` });
         }
@@ -22,7 +24,7 @@ const validatePayload = (req, res, next) => {
         if (!allowedVehicleWheels.includes(req.body.vehicle_wheels.toLowerCase())) {
             return res.status(400).json({ error: `Invalid vehicle_wheels: ${req.body.vehicle_wheels}, it should be ${allowedVehicleWheels.join(", ")}` });
         }
-
+        //validating this are the required fields to work on
         const options = {
             "2": {
                 "models": ["sports", "cruiser"],
@@ -40,14 +42,16 @@ const validatePayload = (req, res, next) => {
                 }
             }
         };
-
+        //throws error if vehicle wheels are other than 2, 4
         const selectedOptions = options[req.body.vehicle_wheels.toLowerCase()];
         if (!selectedOptions) {
             return res.status(400).json({ error: `Invalid vehicle_wheels: ${req.body.vehicle_wheels}` });
         }
+         //throws error if vehicle type error comes
         if (!selectedOptions.models.includes(req.body.vehicle_type.toLowerCase())) {
             return res.status(400).json({ error: `Invalid vehicle_type: ${req.body.vehicle_type}, it should be ${selectedOptions.models.join(", ")}` });
         }
+        //throws error if vehicle name error comes
         const allowedModels = selectedOptions.options[req.body.vehicle_type.toLowerCase()];
         if (!allowedModels || !allowedModels.includes(req.body.vehicle_model.toLowerCase())) {
             return res.status(400).json({ error: `Invalid vehicle_model: ${req.body.vehicle_model}, it should be ${allowedModels.join(", ")}` });
